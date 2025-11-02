03d15e65ec64d388d49e18f0ae27648b14bd3ef9
Delana <dsingh@perse.co.uk>
2025-11-02 20:18:06 +0000

    testing the env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("pendulum")]
+async fn main() {
+    loop {
+        clear_background(BLUE);
+        draw_circle(400.0, 300.0, 10.0, RED);
+        next_frame().await;
+    }
 }
